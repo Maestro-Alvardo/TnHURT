efa572ea3ef5531232a4dba9c5fa6a2431c4bd70
M4357R0 <51530202+Maestro-Alvardo@users.noreply.github.com>
2019-08-26 17:27:21 +0700

    Ini untuk info

---
+++ help.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+import os
+
+print """
+Tools ini tidak berbahaya sama sekali
+Tools ini tidak akan menghapus,mengambil data data anda
+atau yang lainnya
+
+Author : TnHURT
+
+Message:
+	Gunakan Tools ini untuk meretas ya 😉
+	Jangan takut akan apa pun
+	Jika anda menggunakan Tools ini
+	Anda akan menjadi seorang Hacker:v
+
+Dah lah langsung aja ke
+
+python2 Hurt.py
+
+
+"""
